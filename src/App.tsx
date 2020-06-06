4f3f23f7e57071ba833883014dce2fc238ef5e69
Mike Richards <mike.m.richards@gmail.com>
2020-06-06 10:15:49 -0400

    Re-enabled log temporarily.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import * as React from "react"
-import { DBLiveClient } from "@dblive/client-js"
+import { DBLiveClient, DBLiveLogger, DBLiveLoggerLevel } from "@dblive/client-js"
 import { TicTacToeBoardComponent } from "./components/tictactoe.board.component"
+
+DBLiveLogger.logLevel = DBLiveLoggerLevel.debug
 
 export class App extends React.Component
 {
